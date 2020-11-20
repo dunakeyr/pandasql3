6ff85b83c85a0fde683d31a1fc3be8a4eb762b9f
D778598 <robert.dunakey@principal.com>
2020-11-20 15:21:30 -0600

    updating URL

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name="pandasql",
+    name="pandasql3",
     version="0.7.3",
     author="Greg Lamp",
     author_email="greg@yhathq.com",
-    url="https://github.com/yhat/pandasql/",
+    url="https://github.com/dunakeyr/pandasql3/",
     license="MIT",
     packages=find_packages(),
     package_dir={"pandasql": "pandasql"},
